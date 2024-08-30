7b9a7346d2980bd179e4d58f63066c02f5342550
jayakrishnaambavarapu <jkrishnaambavarapu401@gmail.com>
2024-08-30 13:51:20 +0000

    dynamic block example

--- dynamic-blocks.tf
+++ dynamic-blocks.tf
@@ -26,11 +26,11 @@ locals {
 
 resource "aws_security_group" "main" {
    name   = "resource_with_dynamic_block"
-   vpc_id = "vpc-0bbf61891a9cc6e81"
-
+   vpc_id = ""
    dynamic "ingress" {
       for_each = local.ingress_rules
       iterator = ingress-1
+
       content {
          description = ingress-1.value.description
          from_port   = ingress-1.value.port
@@ -44,7 +44,11 @@ resource "aws_security_group" "main" {
       Name = "AWS security group dynamic block"
    }
 }
-
+# iterator temporary variable is ingress-1 is a user defined variable, it takes for_each element and use that in content block to iterate, if iterator argument is not used in dynamic block
+then dynamic name "ingress" is used as a iterator in content block
+# ingress is a block in security group resource, instead of writing manually ingress & egress rules for security groups multiple times we can use dynamic blocks to create ingress blocks at
+run time.
+# dynamic name "ingress" is a valid security group resource name, we need to use valid block names for dynamic blocks
 
 
 
